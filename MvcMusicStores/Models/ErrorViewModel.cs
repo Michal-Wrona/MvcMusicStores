778b1d9deecb0fab6301e01a908070c3e273f46d
Michal-Wrona <michalwro112@gmail.com>
2022-10-25 13:04:03 +0200

    Dodaj pliki projektów.

---
+++ MvcMusicStores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMusicStores.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
